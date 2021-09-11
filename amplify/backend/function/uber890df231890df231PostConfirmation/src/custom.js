d5c761c416d3ca810cdd51df494e0022140b14bd
Rokas <rookas.rudzenskas@gmail.com>
2021-09-11 12:27:01 +0300

    Rokas

--- amplify/backend/function/uber890df231890df231PostConfirmation/src/custom.js
+++ amplify/backend/function/uber890df231890df231PostConfirmation/src/custom.js
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
         Item: {
             'id': { S: event.request.userAttributes.sub },
             '__typename': { S: 'User' },
-            'username': { S: event.userName },
+            'username': { S: event.request.userAttributes.name },
             'email': { S: event.request.userAttributes.email },
             'createdAt': { S: date.toISOString() },
             'updatedAt': { S: date.toISOString() },
