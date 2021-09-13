4d378b956500b621f234291b3c9194ef6d603afe
Rokas <rookas.rudzenskas@gmail.com>
2021-09-13 12:59:04 +0300

    Rokas

---
+++ src/screens/OrderScreen/subscriptions.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onOrderUpdated = /* GraphQL */ `
+    subscription OnOrderUpdated($id: ID!) {
+        onOrderUpdated(id: $id) {
+            
+        }
+    }
+`;
+export const onCarUpdated = /* GraphQL */ `
+    subscription OnCarUpdated($id: ID!) {
+        onCarUpdated(id: $id) {
+            
+        }
+    }
+`;
